a5e15f404c517bbdc3433edd65ed63d08edb0575
lopezproject <geometrybeats@gmail.com>
2025-01-12 16:18:31 -0300

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,12 @@
+document.addEventListener("DOMContentLoaded", () => {
+  const frames = document.querySelectorAll(".frame");
+
+  frames.forEach((frame) => {
+    frame.addEventListener("mouseover", () => {
+      const description = frame.getAttribute("data-description");
+      const text = frame.querySelector("p");
+      text.textContent = description;
+    });
+  });
+});
 
